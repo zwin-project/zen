76d647a660a916588fa1957601f1c5833c8cf4ea
shierote <taishi2060@gmail.com>
2022-02-10 12:10:28 +0900

    [WIP] fix obj viewer

--- clients/obj-viewer/obj-viewer.cc
+++ clients/obj-viewer/obj-viewer.cc
@@ -4,23 +4,18 @@
 #include <string.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/intersect.hpp>
 #include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/gtx/transform.hpp>
 #include <iostream>
 
 #include "types.h"
 
 #define GLSL(str) (const char *)"#version 410\n" #str
-
-// const char *vertex_shader =
-//     GLSL(uniform mat4 model; uniform mat4 zModel; uniform mat4 zVP;
-//          layout(location = 0) in vec3 localPosition;
-//          layout(location = 1) in vec3 normUnnormalized; varying vec4
-//          position; varying vec3 norm; void main() {
-//            norm = normalize(model * vec4(normUnnormalized, 0)).xyz;
-//            position = zModel * model * vec4(localPosition, 1);
-//            gl_Position = zVP * position;
-//          });
 
 const char *vertex_shader = GLSL(
     struct ZLight {
@@ -31,8 +26,11 @@ const char *vertex_shader = GLSL(
     };
     uniform mat4 zModel; uniform mat4 zVP; uniform ZLight zLight;
     uniform mat4 model; layout(location = 0) in vec4 localPosition;
-    layout(location = 1) in vec3 norm; out vec4 frontColor; void main() {
-      vec4 position = zModel * localPosition;
+    layout(location = 1) in vec3 normUnnormalized; out vec4 frontColor;
+    void main() {
+      vec3 norm = normalize(model * vec4(normUnnormalized, 0)).xyz;
+      vec4 position = zModel * (localPosition + vec4(2.0, -6.0, 7.0, 0.0));
+      position.z *= -1;
       vec3 view = -normalize(position.xyz);
       vec3 light = normalize(
           (zLight.position * position.w - zLight.position.w * position).xyz);
@@ -44,21 +42,6 @@ const char *vertex_shader = GLSL(
       gl_Position = zVP * model * position;
     });
 
-// const char *fragment_shader =
-//     GLSL(uniform vec3 ambient; uniform vec3 diffuse; uniform vec3 emissive;
-//          uniform vec3 specular; uniform float shininess; uniform float
-//          opacity; in vec4 v_color;
-
-//          vec3 effectiveDiffuse = diffuse * v_color.rgb;
-//          float effectiveOpacity = opacity * v_color.a;
-
-//          void main() {
-//            gl_FragColor = vec4(emissive + ambient * u_ambientLight +
-//                                    effectiveDiffuse * fakeLight +
-//                                    specular * pow(specularLight, shininess),
-//                effectiveOpacity);
-//          });
-
 const char *fragment_shader = GLSL(
     uniform vec3 ambient; uniform vec3 diffuse; uniform vec3 specular;
     out vec4 outputColor; in vec4 frontColor; void main() {
@@ -69,69 +52,87 @@ ObjViewer::ObjViewer(zukou::App *app, ObjParser *obj_parser)
     : CuboidWindow(app, glm::vec3(1.0)), min_(FLT_MAX), max_(FLT_MIN)
 {
   parser_ = obj_parser;
+  for (auto obj : parser_->obj_list())
+    for (auto mtl_face : obj.faces) {
+      std::cout << mtl_face.first << " ";
+      std::cout << mtl_face.second.size() << " ";
+      std::cout << std::endl;
+    }
 
   for (auto obj : parser_->obj_list()) {
-    std::vector<Vertex> buffer;
-    for (auto face : obj.faces) {
-      for (size_t i = 2; i < face.size(); i++) {
-        ObjFacePoint face_point = face[0];
-        buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
-            parser_->norms()[face_point.norm_index]));
+    for (auto mtl_face : obj.faces) {
+      std::string mtl_name = mtl_face.first;
+      std::vector<ObjFaceLine> faces = mtl_face.second;
 
-        face_point = face[i - 1];
-        buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
-            parser_->norms()[face_point.norm_index]));
+      std::vector<Vertex> buffer;
+      for (auto face : faces) {
+        for (size_t i = 2; i < face.size(); i++) {
+          ObjFacePoint face_point = face[0];
+          buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
+              parser_->norms()[face_point.norm_index]));
 
-        face_point = face[i];
-        buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
-            parser_->norms()[face_point.norm_index]));
+          face_point = face[i - 1];
+          buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
+              parser_->norms()[face_point.norm_index]));
+
+          face_point = face[i];
+          buffer.push_back(Vertex(parser_->vertices()[face_point.vertex_index],
+              parser_->norms()[face_point.norm_index]));
+        }
       }
+
+      zukou::OpenGLComponent *component =
+          new zukou::OpenGLComponent(app_, this);
+      zukou::OpenGLVertexBuffer *vertex_buffer =
+          new zukou::OpenGLVertexBuffer(app_, sizeof(Vertex) * buffer.size());
+      zukou::OpenGLShaderProgram *shader = new zukou::OpenGLShaderProgram(app_);
+
+      if (mtl_name == "" || parser_->mtl_table().count(mtl_name) == 0) {
+        std::cerr << "material not found: " << mtl_name << std::endl;
+        return;
+      }
+
+      MtlObject mtl = parser_->mtl_table()[mtl_name];
+
+      shader->SetVertexShader(vertex_shader, strlen(vertex_shader));
+      shader->SetFragmentShader(fragment_shader, strlen(fragment_shader));
+      shader->Link();
+
+      glm::mat4 model = glm::mat4(1);
+      model = glm::rotate(model, 0.2f, glm::vec3(0, 1, 0));
+      shader->SetUniformVariable("model", model);
+
+      shader->SetUniformVariable("ambient", mtl.ambient);
+      shader->SetUniformVariable("diffuse", mtl.diffuse);
+      // shader->SetUniformVariable("emissive", mtl.emissive);
+      shader->SetUniformVariable("specular", mtl.specular);
+      // shader->SetUniformVariable("shininess", mtl.shininess);
+      // shader->SetUniformVariable("opacity", mtl.opacity);
+
+      component->Attach(shader);
+      component->SetCount(buffer.size());
+      component->SetTopology(ZGN_OPENGL_TOPOLOGY_TRIANGLES);
+      component->AddVertexAttribute(0, 3,
+          ZGN_OPENGL_VERTEX_ATTRIBUTE_TYPE_FLOAT, false, sizeof(Vertex),
+          offsetof(Vertex, point));
+      component->AddVertexAttribute(1, 3,
+          ZGN_OPENGL_VERTEX_ATTRIBUTE_TYPE_FLOAT, false, sizeof(Vertex),
+          offsetof(Vertex, norm));
+
+      {
+        Vertex *data = (Vertex *)vertex_buffer->data();
+        memcpy(data, buffer.data(), sizeof(Vertex) * buffer.size());
+
+        vertex_buffer->BufferUpdated();
+        component->Attach(vertex_buffer);
+      }
+
+      this->component_list_.push_back({
+          component,
+          vertex_buffer,
+          shader,
+      });
     }
-
-    zukou::OpenGLComponent *component = new zukou::OpenGLComponent(app_, this);
-    zukou::OpenGLVertexBuffer *vertex_buffer =
-        new zukou::OpenGLVertexBuffer(app_, sizeof(Vertex) * buffer.size());
-    zukou::OpenGLShaderProgram *shader = new zukou::OpenGLShaderProgram(app_);
-
-    if (obj.mtl_name == "" || parser_->mtl_table().count(obj.mtl_name) == 0) {
-      std::cerr << "material not found: " << obj.mtl_name << std::endl;
-      return;
-    }
-
-    MtlObject mtl = parser_->mtl_table()[obj.mtl_name];
-
-    shader->SetVertexShader(vertex_shader, strlen(vertex_shader));
-    shader->SetFragmentShader(fragment_shader, strlen(fragment_shader));
-    shader->Link();
-    shader->SetUniformVariable("ambient", mtl.ambient);
-    shader->SetUniformVariable("diffuse", mtl.diffuse);
-    // shader->SetUniformVariable("emissive", mtl.emissive);
-    shader->SetUniformVariable("specular", mtl.specular);
-    // shader->SetUniformVariable("shininess", mtl.shininess);
-    // shader->SetUniformVariable("opacity", mtl.opacity);
-    shader->SetUniformVariable("model", glm::mat4(1));
-
-    component->Attach(shader);
-    component->SetCount(buffer.size());
-    component->SetTopology(ZGN_OPENGL_TOPOLOGY_TRIANGLES);
-    component->AddVertexAttribute(0, 3, ZGN_OPENGL_VERTEX_ATTRIBUTE_TYPE_FLOAT,
-        false, sizeof(Vertex), offsetof(Vertex, point));
-    component->AddVertexAttribute(1, 3, ZGN_OPENGL_VERTEX_ATTRIBUTE_TYPE_FLOAT,
-        false, sizeof(Vertex), offsetof(Vertex, norm));
-
-    {
-      Vertex *data = (Vertex *)vertex_buffer->data();
-      memcpy(data, buffer.data(), sizeof(Vertex) * buffer.size());
-
-      vertex_buffer->BufferUpdated();
-      component->Attach(vertex_buffer);
-    }
-
-    this->component_list_.push_back({
-        component,
-        vertex_buffer,
-        shader,
-    });
   }
 }
 
