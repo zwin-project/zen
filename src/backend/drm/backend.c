06b513a3851f8e8356c34b55ddd9befdf357b353
Aki-7 <aki.develop8128@gmail.com>
2022-06-27 00:14:39 +0900

    zen-libinput skeleton

--- src/backend/drm/backend.c
+++ src/backend/drm/backend.c
@@ -1,4 +1,6 @@
+#include <libudev.h>
 #include <zen-backend.h>
+#include <zen-libinput.h>
 #include <zen-session.h>
 #include <zen-util.h>
 
@@ -6,33 +8,49 @@ struct zn_drm_backend {
   struct zn_backend base;
 
   struct zn_session *session;
+  struct udev *udev;
+  struct zn_libinput *input;
 };
 
 ZN_EXPORT struct zn_backend *
 zn_backend_create(struct wl_display *display)
 {
   struct zn_drm_backend *self;
-  struct zn_session *session;
   const char seat_id[] = "seat0";  // FIXME: enable to configure
 
   self = zalloc(sizeof *self);
   if (self == NULL) goto err;
 
-  session = zn_session_create(display);
-  if (session == NULL) {
+  self->session = zn_session_create(display);
+  if (self->session == NULL) {
     zn_log("drm backend: failed to create a session\n");
     goto err_free;
   }
 
-  if (zn_session_connect(session, seat_id) != 0) {
+  if (zn_session_connect(self->session, seat_id) != 0) {
     zn_log("drm backend: session connection failed\n");
     goto err_session;
   }
 
+  self->udev = udev_new();
+  if (self->udev == NULL) {
+    zn_log("drm backend: failed to initialize udev context\n");
+    goto err_session;
+  }
+
+  self->input = zn_libinput_create(self->udev);
+  if (self->input == NULL) {
+    zn_log("drm backend: failed to create input devices\n");
+    goto err_udev;
+  }
+
   return &self->base;
 
+err_udev:
+  udev_unref(self->udev);
+
 err_session:
-  zn_session_destroy(session);
+  zn_session_destroy(self->session);
 
 err_free:
   free(self);
@@ -46,6 +64,8 @@ zn_backend_destroy(struct zn_backend *parent)
 {
   struct zn_drm_backend *self = zn_container_of(parent, self, base);
 
+  zn_libinput_destroy(self->input);
+  udev_unref(self->udev);
   zn_session_destroy(self->session);
   free(self);
 }
