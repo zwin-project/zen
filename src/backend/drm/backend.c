88a69c8370eaf153d3b4066f6a5e9866ae6a0c4b
Aki-7 <aki.develop8128@gmail.com>
2022-06-27 21:17:30 +0900

    bind libinput event to wayland loop

--- src/backend/drm/backend.c
+++ src/backend/drm/backend.c
@@ -27,6 +27,8 @@ zn_backend_create(struct wl_display *display)
     goto err_free;
   }
 
+  zn_log("Press Q to exit...\n");  // FIXME: for dev only, remove this later...
+
   if (zn_session_connect(self->session, seat_id) != 0) {
     zn_log("drm backend: session connection failed\n");
     goto err_session;
@@ -38,7 +40,7 @@ zn_backend_create(struct wl_display *display)
     goto err_session;
   }
 
-  self->input = zn_libinput_create(self->udev, self->session, seat_id);
+  self->input = zn_libinput_create(self->udev, self->session, display, seat_id);
   if (self->input == NULL) {
     zn_log("drm backend: failed to create input devices\n");
     goto err_udev;
--- src/backend/libinput/seat.c
+++ src/backend/libinput/seat.c
@@ -1,11 +1,19 @@
 #include <libinput.h>
+#include <linux/input-event-codes.h>
+#include <stdbool.h>
+#include <wayland-server.h>
 #include <zen-libinput.h>
 #include <zen-session.h>
 #include <zen-util.h>
 
 struct zn_libinput {
+  struct wl_display *display;
   struct zn_session *session;  // nonnull
   struct libinput *libinput;
+
+  struct wl_event_source *libinput_source;
+
+  bool suspended;
 };
 
 static void
@@ -15,6 +23,42 @@ libinput_log_func(struct libinput *libinput,
   UNUSED(libinput);
   UNUSED(priority);
   zn_vlog(format, args);
+}
+
+static void
+zn_libinput_process_events(struct zn_libinput *self)
+{
+  struct libinput_event *event;
+
+  // FIXME: Here, Q key release is detected and the process is terminated.
+  while ((event = libinput_get_event(self->libinput))) {
+    if (libinput_event_get_type(event) == LIBINPUT_EVENT_KEYBOARD_KEY) {
+      struct libinput_event_keyboard *key_event =
+          libinput_event_get_keyboard_event(event);
+      enum libinput_key_state key_state =
+          libinput_event_keyboard_get_key_state(key_event);
+      uint32_t key = libinput_event_keyboard_get_key(key_event);
+
+      if (key_state == LIBINPUT_KEY_STATE_RELEASED && key == KEY_Q)
+        wl_display_terminate(self->display);
+    }
+    libinput_event_destroy(event);
+  }
+}
+
+static int
+zn_libinput_source_dispatch(int fd, uint32_t mask, void *data)
+{
+  struct zn_libinput *self = data;
+  UNUSED(fd);
+  UNUSED(mask);
+
+  if (libinput_dispatch(self->libinput) != 0)
+    zn_log("libinput: failed to dispatch libinput\n");
+
+  zn_libinput_process_events(self);
+
+  return 0;
 }
 
 static int
@@ -38,16 +82,49 @@ static const struct libinput_interface libinput_interface = {
     .close_restricted = zn_libinput_close_restricted,
 };
 
+static int
+zn_libinput_enable(struct zn_libinput *self)
+{
+  struct wl_event_loop *loop;
+  int fd;
+
+  loop = wl_display_get_event_loop(self->display);
+  fd = libinput_get_fd(self->libinput);
+  self->libinput_source = wl_event_loop_add_fd(
+      loop, fd, WL_EVENT_READABLE, zn_libinput_source_dispatch, self);
+  if (self->libinput_source == NULL) return -1;
+
+  if (self->suspended) {
+    if (libinput_resume(self->libinput) != 0) {
+      wl_event_source_remove(self->libinput_source);
+      self->libinput_source = NULL;
+      return -1;
+    }
+    self->suspended = true;
+    zn_libinput_process_events(self);
+  }
+
+  // TODO: notify keyboard focus again
+
+  // TODO: check device existence
+
+  return 0;
+}
+
+// TODO: void zn_libinput_disable(struct zn_libinput *self);
+
 ZN_EXPORT struct zn_libinput *
-zn_libinput_create(
-    struct udev *udev, struct zn_session *session, const char *seat_id)
+zn_libinput_create(struct udev *udev, struct zn_session *session,
+    struct wl_display *display, const char *seat_id)
 {
   struct zn_libinput *self;
   enum libinput_log_priority priority = LIBINPUT_LOG_PRIORITY_INFO;
-  UNUSED(udev);
 
   self = zalloc(sizeof *self);
   if (self == NULL) goto err;
+
+  self->session = session;
+  self->display = display;
 
   self->libinput =
       libinput_udev_create_context(&libinput_interface, self, udev);
@@ -62,9 +139,9 @@ zn_libinput_create(
     goto err_unref;
   }
 
-  // TODO: There is more to be done
+  zn_libinput_process_events(self);
 
-  self->session = session;
+  zn_libinput_enable(self);
 
   return self;
 
@@ -81,6 +158,7 @@ err:
 ZN_EXPORT void
 zn_libinput_destroy(struct zn_libinput *self)
 {
+  if (self->libinput_source) wl_event_source_remove(self->libinput_source);
   libinput_unref(self->libinput);
   free(self);
 }
