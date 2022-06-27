fd86d81ced9de34d7f4277c9ed25a05d9124eb3f
Aki-7 <aki.develop8128@gmail.com>
2022-06-27 20:08:47 +0900

    libinput log handler

--- zen-util/src/log.c
+++ zen-util/src/log.c
@@ -10,8 +10,14 @@ zn_log(const char *fmt, ...)
   va_list argp;
 
   va_start(argp, fmt);
-  l = vfprintf(stderr, fmt, argp);
+  l = zn_vlog(fmt, argp);
   va_end(argp);
 
   return l;
+}
+
+ZN_EXPORT int
+zn_vlog(const char *fmt, va_list ap)
+{
+  return vfprintf(stderr, fmt, ap);
 }
