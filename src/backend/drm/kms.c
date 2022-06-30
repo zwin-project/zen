dc4b8501c210dc7867c730da2534e555ce05cc70
Aki-7 <aki.develop8128@gmail.com>
2022-06-30 18:22:29 +0900

    check and set kms capabilities

--- src/backend/drm/kms.c
+++ src/backend/drm/kms.c
@@ -1,6 +1,55 @@
 #include "kms.h"
 
+#include <xf86drm.h>
+#include <xf86drmMode.h>
 #include <zen-util.h>
+
+static int
+zn_kms_init_caps(struct zn_kms *self)
+{
+  uint64_t cap;
+  int ret;
+
+  ret = drmGetCap(self->drm_fd, DRM_CAP_TIMESTAMP_MONOTONIC, &cap);
+  self->caps.timestamp_monotonic = ((ret == 0) && (cap == 1));
+
+  if (!self->caps.timestamp_monotonic) {
+    zn_log(
+        "DRM Error: kernel DRM KMS does not support "
+        "DRM_CAP_TIMESTAMP_MONOTONIC\n");
+    return -1;
+  }
+
+  // TODO: set CLOCK_MONOTONIC for presentation clock
+
+  ret = drmGetCap(self->drm_fd, DRM_CAP_CURSOR_WIDTH, &cap);
+  if (ret == 0)
+    self->caps.cursor_width = cap;
+  else
+    self->caps.cursor_width = 64;
+
+  ret = drmGetCap(self->drm_fd, DRM_CAP_CURSOR_HEIGHT, &cap);
+  if (ret == 0)
+    self->caps.cursor_height = cap;
+  else
+    self->caps.cursor_height = 64;
+
+  ret = drmSetClientCap(self->drm_fd, DRM_CLIENT_CAP_UNIVERSAL_PLANES, 1);
+  if (ret != 0) {
+    zn_log("DRM: failed to enable DRM_CLIENT_CAP_UNIVERSAL_PLANES\n");
+    return -1;
+  }
+
+  ret = drmGetCap(self->drm_fd, DRM_CAP_CRTC_IN_VBLANK_EVENT, &cap);
+  if (ret != 0) cap = 0;
+  ret = drmSetClientCap(self->drm_fd, DRM_CLIENT_CAP_ATOMIC, 1);
+  self->caps.atomic_modeset = ((ret == 0) && (cap == 1));
+
+  zn_log("DRM: %s atomic modesetting\n",
+      self->caps.atomic_modeset ? "supports" : "does not support");
+
+  return 0;
+}
 
 struct zn_kms *
 zn_kms_create(int drm_fd)
@@ -12,7 +61,12 @@ zn_kms_create(int drm_fd)
 
   self->drm_fd = drm_fd;
 
+  if (zn_kms_init_caps(self) != 0) goto err_free;
+
   return self;
+
+err_free:
+  free(self);
 
 err:
   return NULL;
--- src/backend/drm/kms.h
+++ src/backend/drm/kms.h
@@ -1,11 +1,18 @@
 #ifndef ZEN_DRM_BACKEND_KMS_H
 #define ZEN_DRM_BACKEND_KMS_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 /* zn_drm_backend owns zn_kms */
 struct zn_kms {
   int drm_fd;  // managed by zn_drm_backend
+  struct {
+    int32_t cursor_width;
+    int32_t cursor_height;
+    bool timestamp_monotonic;
+    bool atomic_modeset;
+  } caps;
 };
 
 struct zn_kms *zn_kms_create(int drm_fd);
