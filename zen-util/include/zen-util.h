51ee68d435db15a08a311e4f94fe4aa745b0d51d
Aki-7 <aki.develop8128@gmail.com>
2022-07-11 20:59:34 +0900

    initial commit

---
+++ zen-util/include/zen-util.h
@@ -0,0 +1,45 @@
+#ifndef ZEN_UTIL_H
+#define ZEN_UTIL_H
+
+#include <assert.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/** Visibility attribute */
+#if defined(__GNUC__) && __GNUC__ >= 4
+#define ZN_EXPORT __attribute__((visibility("default")))
+#else
+#define ZN_EXPORT
+#endif
+
+/** Compile-time computation of number of items in an array */
+#ifndef ARRAY_LENGTH
+#define ARRAY_LENGTH(a) (sizeof(a) / sizeof(a)[0])
+#endif
+
+/** Suppress compiler warnings for unused variables */
+#ifndef UNUSED
+#define UNUSED(x) ((void)x)
+#endif
+
+/** Allocate memory and set to zero */
+static inline void *
+zalloc(size_t size)
+{
+  return calloc(1, size);
+}
+
+/** Retrieve a pointer to a containing struct */
+#define zn_container_of(ptr, sample, member) \
+  (__typeof__(sample))((char *)(ptr)-offsetof(__typeof__(*sample), member))
+
+/** logger */
+int zn_log(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
+/* alternative logger api */
+int zn_vlog(const char *fmt, va_list ap);
+
+#endif  //  ZEN_UTIL_H
