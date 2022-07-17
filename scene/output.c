93711fc2d10993171f08c01b0ce0f1cd8cd2c37f
Aki-7 <aki.develop8128@gmail.com>
2022-07-17 22:49:26 +0900

    send frame done for each surface

--- scene/output.c
+++ scene/output.c
@@ -1,8 +1,21 @@
 #include "output.h"
 
 #include "scene.h"
+#include "toplevel-view.h"
 #include "zen-common.h"
 #include "zen-scene.h"
+
+void
+zn_scene_output_for_each_surface(struct zn_scene_output* self,
+    void (*iterator)(struct wlr_surface* surface, void* data), void* data)
+{
+  struct zn_scene_toplevel_view* toplevel;
+  wl_list_for_each(toplevel, &self->toplevels, link)
+  {
+    iterator(toplevel->wlr_xdg_toplevel->base->surface, data);
+    // TODO: iterate popups and other things
+  }
+}
 
 struct zn_scene_output*
 zn_scene_output_create(struct zn_scene* scene, struct wlr_output* wlr_output)
