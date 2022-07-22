293d92b96c3451c24ada5fb86a7db6bd65509306
Aki-7 <aki.develop8128@gmail.com>
2022-07-22 19:42:38 +0900

    create a tentative protocol to switch desktop modes

---
+++ tests/validate-xml-test.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <libxml/parser.h>
+
+#include "test-runner.h"
+
+void
+validate_xml(const char *xml_path)
+{
+  int success;
+  const char *wayland_dtd_path = getenv("WAYLAND_DTD_PATH");
+  assert(wayland_dtd_path && xml_path);
+
+  xmlParserCtxtPtr ctx = NULL;
+  xmlDocPtr doc = NULL;
+  xmlDtdPtr dtd = NULL;
+  xmlValidCtxtPtr dtdctx;
+  xmlParserInputBufferPtr buffer;
+
+  dtdctx = xmlNewValidCtxt();
+  ctx = xmlNewParserCtxt();
+  assert(dtdctx && ctx);
+
+  buffer = xmlParserInputBufferCreateFilename(
+      wayland_dtd_path, XML_CHAR_ENCODING_UTF8);
+  assert(buffer);
+
+  dtd = xmlIOParseDTD(NULL, buffer, XML_CHAR_ENCODING_UTF8);
+  doc = xmlCtxtReadFile(ctx, xml_path, NULL, 0);
+  assert(doc && dtd);
+
+  success = xmlValidateDtd(dtdctx, doc, dtd);
+  xmlFreeDoc(doc);
+  xmlFreeParserCtxt(ctx);
+  xmlFreeDtd(dtd);
+  xmlFreeValidCtxt(dtdctx);
+  assert(success);
+}
+
+TEST(validate_zigen_xml)
+{
+  const char *zigen_xml = getenv("ZEN_DESKTOP_XML");
+  validate_xml(zigen_xml);
+}
