bc31b812ad9831b80512c8af0fbe2cd90ef23bf4
watasuke102 <watasuke102@gmail.com>
2022-07-28 09:06:15 +0900

    remove zn_screen_layout_reconfigure()
    it isn't necessary now

--- zen/scene/screen-layout.c
+++ zen/scene/screen-layout.c
@@ -3,22 +3,11 @@
 #include "zen-common.h"
 #include "zen/scene/screen.h"
 
-static void
-zn_screen_layout_reconfigure(struct zn_screen_layout* self)
-{
-  struct zn_screen* screen;
-  wl_list_for_each(screen, &self->screens, link)
-  {
-    // TODO
-  }
-}
-
 void
 zn_screen_layout_add_auto(
     struct zn_screen_layout* self, struct zn_screen* screen)
 {
   wl_list_insert(&self->screens, &screen->link);
-  zn_screen_layout_reconfigure(self);
 }
 
 struct zn_screen_layout*
