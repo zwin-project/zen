33fe6e513662e0fbe4c8ec38e6bed761e6f6fb46
watasuke102 <watasuke102@gmail.com>
2022-07-28 10:10:37 +0900

    fix existing process about screen

--- zen/xwayland-view.c
+++ zen/xwayland-view.c
@@ -1,6 +1,7 @@
 #include "zen/xwayland-view.h"
 
 #include "zen-common.h"
+#include "zen/scene/scene.h"
 #include "zen/scene/view.h"
 
 struct zn_xwayland_view {
@@ -27,13 +28,13 @@ zn_xwayland_view_map(struct wl_listener* listener, void* data)
   if (!zn_assert(!zn_view_is_mapped(&self->base), "Tried to map a mapped view"))
     return;
 
-  if (wl_list_empty(&scene->screens)) {
+  if (wl_list_empty(&scene->screen_layout->screens)) {
     // TODO: handle this properly
     zn_warn("View is mapped but no screen found");
     return;
   }
 
-  screen = zn_container_of(scene->screens.next, screen, link);
+  screen = zn_container_of(scene->screen_layout->screens.next, screen, link);
 
   zn_view_map_to_screen(&self->base, screen);
 }
