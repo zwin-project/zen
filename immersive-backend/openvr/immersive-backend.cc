dd39563d917236ee3f17bda474fa95ecb052afee
Aki-7 <aki.develop8128@gmail.com>
2022-07-31 01:21:08 +0900

    repaint loop with openvr

--- immersive-backend/openvr/immersive-backend.cc
+++ immersive-backend/openvr/immersive-backend.cc
@@ -1,3 +1,5 @@
+#include <wayland-server-core.h>
+
 #include <memory>
 
 #include "vr-system.h"
@@ -8,6 +10,22 @@ struct zn_openvr_immersive_backend {
   struct zn_immersive_backend base;
   std::unique_ptr<zen::VrSystem> vr_system;  // nonnull
 };
+
+static void
+zn_openvr_immersive_backend_vr_system_disconnected_handler(
+    struct zn_openvr_immersive_backend* self)
+{
+  wl_signal_emit(&self->base.events.disconnected, NULL);
+}
+
+void
+zn_immersive_backend_start_repaint_loop(struct zn_immersive_backend* parent)
+{
+  struct zn_openvr_immersive_backend* self =
+      zn_container_of(parent, self, base);
+
+  self->vr_system->StartRepaintLoop();
+}
 
 bool
 zn_immersive_backend_connect(struct zn_immersive_backend* parent)
@@ -28,7 +46,7 @@ zn_immersive_backend_disconnect(struct zn_immersive_backend* parent)
 }
 
 struct zn_immersive_backend*
-zn_immersive_backend_create()
+zn_immersive_backend_create(struct wl_event_loop* loop)
 {
   struct zn_openvr_immersive_backend* self;
 
@@ -39,8 +57,20 @@ zn_immersive_backend_create()
   }
 
   self->vr_system = std::make_unique<zen::VrSystem>();
+  if (self->vr_system->Init(loop) == false) {
+    zn_error("Failed to init vr system");
+    goto err_free;
+  }
+
+  self->vr_system->callbacks.Disconnected = std::bind(
+      zn_openvr_immersive_backend_vr_system_disconnected_handler, self);
+
+  wl_signal_init(&self->base.events.disconnected);
 
   return &self->base;
+
+err_free:
+  free(self);
 
 err:
   return NULL;
