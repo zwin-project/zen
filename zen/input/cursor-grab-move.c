ee0ac2c884a166b35b2b8fcfce31f20ca5b16cdf
watasuke102 <watasuke102@gmail.com>
2022-08-30 22:13:02 +0900

    add zn_cursor_grab_move

---
+++ zen/input/cursor-grab-move.c
@@ -0,0 +1,70 @@
+#include "zen/input/cursor-grab-move.h"
+
+#include "zen-common.h"
+
+void zn_cursor_grab_move_end(struct zn_cursor_grab_move* self);
+
+static void
+move_grab_motion(
+    struct zn_cursor_grab* grab, struct wlr_event_pointer_motion* event)
+{
+  UNUSED(grab);
+  UNUSED(event);
+}
+
+static void
+move_grab_button(
+    struct zn_cursor_grab* grab, struct wlr_event_pointer_button* event)
+{
+  struct zn_cursor_grab_move* self = zn_container_of(grab, self, base);
+
+  if (event->state == WLR_BUTTON_RELEASED) {
+    zn_cursor_grab_move_end(self);
+  }
+}
+
+static void
+move_grab_axis(
+    struct zn_cursor_grab* grab, struct wlr_event_pointer_axis* event)
+{
+  UNUSED(grab);
+  UNUSED(event);
+}
+
+static void
+move_grab_frame(struct zn_cursor_grab* grab)
+{
+  UNUSED(grab);
+}
+
+static const struct zn_cursor_grab_interface move_grab_interface = {
+    .motion = move_grab_motion,
+    .button = move_grab_button,
+    .axis = move_grab_axis,
+    .frame = move_grab_frame,
+};
+
+void
+zn_cursor_grab_move_end(struct zn_cursor_grab_move* self)
+{
+  zn_cursor_end_grab(self->base.cursor);
+  free(self);
+}
+
+void
+zn_cursor_grab_move_start(struct zn_cursor* cursor, struct zn_view* view)
+{
+  struct zn_cursor_grab_move* self;
+  self = zalloc(sizeof *self);
+
+  if (self == NULL) {
+    zn_error("Failed to allocate memory");
+    return;
+  }
+
+  self->view = view;
+  self->base.interface = &move_grab_interface;
+  self->base.cursor = cursor;
+
+  zn_cursor_start_grab(cursor, &self->base);
+}
