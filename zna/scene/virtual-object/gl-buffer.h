5a0f2fc8e198bdb07752a486a9bb7b061855304f
Aki-7 <aki.develop8128@gmail.com>
2022-11-12 21:51:48 +0900

    apply gl-buffer commit

--- zna/scene/virtual-object/gl-buffer.h
+++ zna/scene/virtual-object/gl-buffer.h
@@ -14,8 +14,13 @@ struct zna_gl_buffer {
 
   struct wl_listener zgnr_gl_buffer_destroy_listener;
   struct wl_listener session_destroyed_listener;
-  struct wl_listener session_created_listener;
 };
+
+/**
+ * Precondition:
+ *  Current session exists && the zgnr_gl_buffer has been committed
+ */
+void zna_gl_buffer_apply_commit(struct zna_gl_buffer *self, bool only_damaged);
 
 struct zna_gl_buffer *zna_gl_buffer_create(
     struct zgnr_gl_buffer *zgnr_gl_buffer, struct zna_system *system);
