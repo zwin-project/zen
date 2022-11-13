ed1b7ed7d840cf9609413b599f877d4fbcd94301
Aki-7 <aki.develop8128@gmail.com>
2022-11-13 16:59:19 +0900

    Create znr_gl_base_technique on applying commit

--- zna/scene/virtual-object/gl-base-technique.h
+++ zna/scene/virtual-object/gl-base-technique.h
@@ -3,10 +3,17 @@
 #include <zgnr/gl-base-technique.h>
 
 #include "system.h"
+#include "zen/renderer/gl-base-technique.h"
 
 struct zna_gl_base_technique {
   struct zgnr_gl_base_technique* zgnr_gl_base_technique;  // nonnull
   struct zna_system* system;                              // nonnull
+
+  /**
+   * null before the initial commit or when the current session does not exists,
+   * nonnull otherwise
+   */
+  struct znr_gl_base_technique* znr_gl_base_technique;
 
   struct wl_listener zgnr_gl_base_technique_destroy_listener;
   struct wl_listener session_destroyed_listener;
@@ -14,7 +21,8 @@ struct zna_gl_base_technique {
 
 /**
  * Precondition:
- *  Current session exists && the zgnr_gl_base_technique has been commited
+ *  Current session exists && the zgnr_gl_base_technique has been commited &&
+ * parent zna_rendering_unit has a valid znr_rendering_unit
  */
 void zna_gl_base_technique_apply_commit(
     struct zna_gl_base_technique* self, bool only_damaged);
