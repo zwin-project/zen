f42580fbf6c2ebaf8744a356a3f4b88f9a2d63a8
Aki-7 <aki.develop8128@gmail.com>
2022-11-30 19:06:16 +0900

    znr-remote

---
+++ include/zen/renderer/virtual-object.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cglm/types.h>
+
+#include "zen/renderer/session.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct znr_virtual_object;
+
+void znr_virtual_object_move(
+    struct znr_virtual_object* self, vec3 position, versor quaternion);
+
+void znr_virtual_object_commit(struct znr_virtual_object* self);
+
+struct znr_virtual_object* znr_virtual_object_create(
+    struct znr_session* session);
+
+void znr_virtual_object_destroy(struct znr_virtual_object* self);
+
+#ifdef __cplusplus
+}
+#endif
