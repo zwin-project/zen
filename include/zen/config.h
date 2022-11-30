5befc7737c589e9b406a55d329433fa3be0d28ff
Aki-7 <aki.develop8128@gmail.com>
2022-11-30 14:51:28 +0900

    clear zen

--- include/zen/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct zn_config {
-  char* bg_image_file;  // can be empty string but cannot be null
-};
-
-struct zn_config* zn_config_create(void);
-
-void zn_config_destroy(struct zn_config* self);
