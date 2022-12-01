a9e3f40f5567278aa7a711a31e699571d3a9826b
Aki-7 <aki.develop8128@gmail.com>
2022-12-01 18:23:15 +0900

    restore immersive display system

---
+++ zen/input/seat.c
@@ -0,0 +1,110 @@
+#include "zen/input/seat.h"
+
+#include <wlr/types/wlr_seat.h>
+#include <zigen-protocol.h>
+
+#include "zen-common.h"
+#include "zen/input/input-device.h"
+
+static void
+zn_seat_update_capabilities(struct zn_seat *self)
+{
+  uint32_t wl_caps = 0;
+  uint32_t zgn_caps = 0;
+
+  struct zn_input_device *input_device;
+  wl_list_for_each (input_device, &self->devices, link) {
+    switch (input_device->wlr_input->type) {
+      case WLR_INPUT_DEVICE_KEYBOARD:
+        wl_caps |= WL_SEAT_CAPABILITY_KEYBOARD;
+        break;
+      case WLR_INPUT_DEVICE_POINTER:
+        wl_caps |= WL_SEAT_CAPABILITY_POINTER;
+        zgn_caps |= ZGN_SEAT_CAPABILITY_RAY_DIRECTION;
+        break;
+      case WLR_INPUT_DEVICE_TOUCH:
+        // TODO: support touch device
+        break;
+      case WLR_INPUT_DEVICE_TABLET_TOOL:
+      case WLR_INPUT_DEVICE_TABLET_PAD:
+      case WLR_INPUT_DEVICE_SWITCH:
+        break;
+    }
+  }
+
+  wlr_seat_set_capabilities(self->wlr_seat, wl_caps);
+  zgnr_seat_set_capabilities(self->zgnr_seat, zgn_caps);
+}
+
+void
+zn_seat_add_device(struct zn_seat *self, struct zn_input_device *input_device)
+{
+  wl_list_insert(&self->devices, &input_device->link);
+  zn_seat_update_capabilities(self);
+}
+
+void
+zn_seat_remove_device(
+    struct zn_seat *self, struct zn_input_device *input_device)
+{
+  wl_list_remove(&input_device->link);
+  zn_seat_update_capabilities(self);
+}
+
+struct zn_seat *
+zn_seat_create(struct wl_display *display, const char *seat_name)
+{
+  struct zn_seat *self;
+  self = zalloc(sizeof *self);
+  if (self == NULL) {
+    zn_error("Failed to allocate memory");
+    goto err;
+  }
+
+  self->wlr_seat = wlr_seat_create(display, seat_name);
+  if (self->wlr_seat == NULL) {
+    zn_error("Failed to create wlr_seat");
+    goto err_free;
+  }
+
+  self->zgnr_seat = zgnr_seat_create(display);
+  if (self->zgnr_seat == NULL) {
+    zn_error("Failed to creat zgnr_seat");
+    goto err_wlr_seat;
+  }
+
+  self->ray = zn_ray_create();
+  if (self->ray == NULL) {
+    zn_error("Failed to create zn_ray");
+    goto err_zgnr_seat;
+  }
+
+  wl_list_init(&self->devices);
+  wl_signal_init(&self->events.destroy);
+
+  return self;
+
+err_zgnr_seat:
+  zgnr_seat_destroy(self->zgnr_seat);
+
+err_wlr_seat:
+  wlr_seat_destroy(self->wlr_seat);
+
+err_free:
+  free(self);
+
+err:
+  return NULL;
+}
+
+void
+zn_seat_destroy(struct zn_seat *self)
+{
+  wl_signal_emit(&self->events.destroy, NULL);
+
+  wl_list_remove(&self->devices);
+  zn_ray_destroy(self->ray);
+  zgnr_seat_destroy(self->zgnr_seat);
+  wlr_seat_destroy(self->wlr_seat);
+  free(self);
+}
---
+++ zen/virtual-object.c
@@ -0,0 +1,84 @@
+#include "zen/virtual-object.h"
+
+#include <cglm/quat.h>
+#include <cglm/vec3.h>
+#include <zen-common.h>
+
+#include "zen/appearance/virtual-object.h"
+#include "zen/server.h"
+
+static void zn_virtual_object_destroy(struct zn_virtual_object *self);
+
+static void
+zn_virtual_object_handle_zgnr_virtual_object_destroy(
+    struct wl_listener *listener, void *data)
+{
+  UNUSED(data);
+  struct zn_virtual_object *self =
+      zn_container_of(listener, self, zgnr_virtual_object_destroy_listener);
+
+  zn_virtual_object_destroy(self);
+}
+
+void
+zn_virtual_object_move(
+    struct zn_virtual_object *self, vec3 position, versor quaternion)
+{
+  glm_vec3_copy(position, self->position);
+  glm_vec4_copy(quaternion, self->quaternion);
+  glm_mat4_identity(self->model_matrix);
+  glm_translate(self->model_matrix, position);
+  glm_quat_rotate(self->model_matrix, quaternion, self->model_matrix);
+  glm_mat4_inv(self->model_matrix, self->model_invert);
+  wl_signal_emit(&self->events.move, NULL);
+}
+
+struct zn_virtual_object *
+zn_virtual_object_create(struct zgnr_virtual_object *zgnr_virtual_object)
+{
+  struct zn_virtual_object *self;
+  struct zn_server *server = zn_server_get_singleton();
+
+  self = zalloc(sizeof *self);
+  if (self == NULL) {
+    zn_error("Failed to allocate memory");
+    goto err;
+  }
+
+  self->zgnr_virtual_object = zgnr_virtual_object;
+  zgnr_virtual_object->user_data = self;
+
+  wl_list_init(&self->link);
+  wl_signal_init(&self->events.move);
+
+  self->zgnr_virtual_object_destroy_listener.notify =
+      zn_virtual_object_handle_zgnr_virtual_object_destroy;
+  wl_signal_add(&self->zgnr_virtual_object->events.destroy,
+      &self->zgnr_virtual_object_destroy_listener);
+
+  self->appearance = zna_virtual_object_create(self, server->appearance_system);
+  if (self->appearance == NULL) {
+    goto err_free;
+  }
+
+  vec3 initial_pos = {0, 1, -1};
+  zn_virtual_object_move(self, initial_pos, GLM_QUAT_IDENTITY);
+
+  return self;
+
+err_free:
+  free(self);
+
+err:
+  return NULL;
+}
+
+static void
+zn_virtual_object_destroy(struct zn_virtual_object *self)
+{
+  zna_virtual_object_destroy(self->appearance);
+  wl_list_remove(&self->events.move.listener_list);
+  wl_list_remove(&self->link);
+  wl_list_remove(&self->zgnr_virtual_object_destroy_listener.link);
+  free(self);
+}
