fe6c2e89711cf6f062147e6ad16f7179c7375046
watasuke102 <watasuke102@gmail.com>
2022-12-10 21:48:03 +0900

    fix formatting

--- include/zen/wlr/box.h
+++ include/zen/wlr/box.h
@@ -2,5 +2,5 @@
 
 #include <wlr/util/box.h>
 
-void zn_wlr_fbox_closest_point(const struct wlr_fbox* box, double x, double y,
-    double* dest_x, double* dest_y);
+void zn_wlr_fbox_closest_point(const struct wlr_fbox *box, double x, double y,
+    double *dest_x, double *dest_y);
