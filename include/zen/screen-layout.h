55bc33ce25a6f037fd93b1a17b00391804b3d4c9
watasuke102 <watasuke102@gmail.com>
2022-12-11 12:05:27 +0900

    add comment to some member/function

--- include/zen/screen-layout.h
+++ include/zen/screen-layout.h
@@ -10,7 +10,7 @@ struct zn_screen_layout {
 };
 
 // Returns the screen at the given x, y of the coordinates of screen layout.
-// And sets the screen-local coordinates to dst_{x,y}
+// And sets the screen-local coordinates to dest_{x,y}
 struct zn_screen *zn_screen_layout_get_closest_screen(
     struct zn_screen_layout *self, double x, double y, double *dest_x,
     double *dest_y);
