f52cb6dc548f76e3408b3ee08e108fd57739ceb8
Koki Ryu <kokiryu@google.com>
2022-12-30 00:59:36 +0900

    Add system_font to zigzag_layout

--- zigzag/src/layout.c
+++ zigzag/src/layout.c
@@ -1,3 +1,4 @@
+#include <cairo-ft.h>
 #include <stdlib.h>
 #include <wayland-server-core.h>
 #include <zen-common.h>
@@ -5,7 +6,8 @@
 
 struct zigzag_layout *
 zigzag_layout_create(const struct zigzag_layout_impl *implementation,
-    int output_width, int output_height, void *user_data)
+    int output_width, int output_height, const char *font_file_path,
+    void *user_data)
 {
   struct zigzag_layout *self;
   self = zalloc(sizeof *self);
@@ -20,9 +22,36 @@ zigzag_layout_create(const struct zigzag_layout_impl *implementation,
 
   self->implementation = implementation;
 
+  FT_Library library;
+  FT_Init_FreeType(&library);
+  FT_Face ft_face;
+
+  if (FT_New_Face(library, font_file_path, 0, &ft_face) == 0) {
+    self->system_font = cairo_ft_font_face_create_for_ft_face(ft_face, 0);
+    cairo_status_t status = cairo_font_face_set_user_data(self->system_font,
+        &self->ft_font_face_key, ft_face, (cairo_destroy_func_t)FT_Done_Face);
+    if (status) {
+      zn_error("Failed to bind the FT_Face to cairo_font_face_t");
+      goto err_font;
+    }
+    status = cairo_font_face_set_user_data(self->system_font,
+        &self->ft_library_key, library, (cairo_destroy_func_t)FT_Done_FreeType);
+    if (status) {
+      zn_error("Failed to bind the FT_Library to cairo_font_face_t");
+      goto err_font;
+    }
+  } else {
+    self->system_font = cairo_toy_font_face_create(
+        "sans-serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+  }
+
   wl_list_init(&self->node_list);
 
   return self;
+err_font:
+  cairo_font_face_destroy(self->system_font);
+  FT_Done_Face(ft_face);
+  FT_Done_FreeType(library);
 err:
   return NULL;
 }
@@ -31,5 +60,6 @@ void
 zigzag_layout_destroy(struct zigzag_layout *self)
 {
   wl_list_remove(&self->node_list);
+  cairo_font_face_destroy(self->system_font);
   free(self);
 }
