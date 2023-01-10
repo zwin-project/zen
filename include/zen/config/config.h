6ba499c0081bf1d5a6f31c16a8ce7a7a591e8cbf
Koki Ryu <kokiryu@google.com>
2023-01-11 01:25:14 +0900

    Use favorite-apps in config to generate 2d launchers

--- include/zen/config/config.h
+++ include/zen/config/config.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <toml.h>
+struct zn_favorite_app;
 
 struct zn_config {
   char *space_default_app;   // nonnull, non-empty
   char *wallpaper_filepath;  // can be empty string but cannot be null
   int64_t board_initial_count;
+  int64_t num_favorite_apps;
+  struct zn_favorite_app *favorite_apps;
 };
 
 struct zn_config *zn_config_create(struct toml_table_t *config_table);
--- include/zen/ui/nodes/app-launcher.h
+++ include/zen/ui/nodes/app-launcher.h
@@ -4,16 +4,12 @@
 #include <wlr/render/wlr_renderer.h>
 #include <zigzag.h>
 
-struct zn_app_launcher_data {
-  char *icon_path;
-  char *command;
-};
+struct zn_favorite_app;
 
 struct zn_app_launcher {
   struct zigzag_node *zigzag_node;
 
-  const struct zn_app_launcher_data
-      *data;  // not owning, pointing to an element of default_launchers
+  struct zn_favorite_app *app;  // not owning
 
   struct wl_list link;  // for zn_menu_bar::launcher_list
   cairo_surface_t *launcher_icon_surface;
@@ -21,7 +17,6 @@ struct zn_app_launcher {
 };
 
 struct zn_app_launcher *zn_app_launcher_create(
-    struct zigzag_layout *zigzag_layout,
-    const struct zn_app_launcher_data *data, int idx);
+    struct zigzag_layout *zigzag_layout, struct zn_favorite_app *app, int idx);
 
 void zn_app_launcher_destroy(struct zn_app_launcher *self);
