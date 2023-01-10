5ee3d08ed927cb7b2551c25b6167bfeb9b08f502
Aki-7 <aki.develop8128@gmail.com>
2023-01-10 23:27:13 +0900

    ray origin appearance

--- zna/ray/origin-unit.c
+++ zna/ray/origin-unit.c
@@ -1,8 +1,177 @@
 #include "origin-unit.h"
 
 #include <GLES3/gl32.h>
+#include <cglm/affine.h>
+#include <cglm/mat4.h>
+#include <cglm/vec2.h>
+#include <cglm/vec3.h>
 
 #include "zen-common.h"
+
+/**
+ * * Rotate around z axis
+ * * Origin is identical with ray's origin
+ * * -z direction is identical with ray's direction
+ * * Between north and south poll, it is divided into RESOLUTION_Z fragments.
+ * * z value of north poll is CURVE_MAX_Z
+ * * z value of south poll is CURVE_MIN_Z
+ *
+ *               ^r
+ *               |
+ *               |       r = curve_function(z);
+ *          _____|___
+ *         /     |   \___
+ *        /      |       ---_____
+ * z<----|-------|----------------\-----
+ *     north     |              south
+ *     pole      |              poll
+ *               |
+ *
+ * The north and south poles and the longitudinal line at the texture juncture
+ * have duplicated vertices. The number of total vertices is
+ *
+ *            (RESOLUTION_THETA + 1) * (RESOLUTION_Z + 1)
+ *
+ * ex) RESOLUTION_THETA = 8, RESOLUTION_Z = 6
+ *
+ * i=0 v=0  0----1----2----3----4----5----6----7----8  north pole
+ * i=1      9----10---11---12---13---14---15---16---17
+ * i=2      18---19---20---21---22---23---24---25---26
+ * i=3      27---28---29---30---31---32---33---34---35
+ * i=4      36---37---38---39---40---41---42---43---44
+ * i=5      45---46---47---48---49---50---51---52---53
+ * i=6 v=1  54---55---56---57---58---59---60---61---62 south pole
+ *
+ *          j=0  j=1  j=2  j=3  j=4  j=5  j=6  j=7  j=8
+ *          u=0                                     u=1
+ *
+ * Vertices are stored in this order.
+ * We use i,n,j like above to represent longitudinal and latitudinal lines.
+ *
+ */
+
+#define CURVE_MIN_Z (-0.08f)
+#define CURVE_MAX_Z (+0.02f)
+#define RESOLUTION_Z 50
+#define RESOLUTION_THETA 25
+
+static const int32_t vertex_count = (RESOLUTION_THETA + 1) * (RESOLUTION_Z + 1);
+static const int32_t element_count =
+    3 * (RESOLUTION_THETA * 2) * (RESOLUTION_Z - 1);
+
+struct zna_ray_origin_unit_vertex {
+  vec3 position;
+  vec3 normal;
+  vec2 uv;
+};
+
+static float
+curve_function(float z)
+{
+  if (-0.08 < z && z <= -0.04551448) {
+    return sqrtf(powf(0.04, 2) - powf(z + 0.04, 2)) / 6.f;
+  } else if (-0.04551448 <= z && z <= -0.015059523) {
+    return -0.0001 / (z + 0.0039) + 0.0042;
+  } else if (-0.015059523 <= z && z <= 0.02) {
+    return sqrtf(powf(0.02, 2) - powf(z, 2));
+  } else {
+    return 0.f;
+  }
+}
+
+static float
+z_resolution_density(float a)
+{
+  if (0 <= a && a <= 0.2) {
+    return 0.25 * a;
+  } else if (0.2 <= a && a <= 0.8) {
+    return 1.5 * a - 0.25;
+  } else if (0.8 <= a && a <= 1.0) {
+    return 0.25 * a + 0.75;
+  } else {
+    return 0;
+  }
+}
+
+static void
+construct_vertices(struct zna_ray_origin_unit_vertex *vertices)
+{
+  float dz = 1.f / (float)RESOLUTION_Z / 10.f;
+
+  for (int i = 0; i <= RESOLUTION_Z; i++) {
+    float z, r, dr;
+    vec2 normal_zr;  // normal in zr-plane
+    float a = (float)i / (float)RESOLUTION_Z;
+    a = z_resolution_density(a);
+    z = CURVE_MAX_Z * (1.f - a) + CURVE_MIN_Z * a;
+    r = curve_function(z);
+    dr = curve_function(z + dz) - r;
+
+    normal_zr[0] = -dr;
+    normal_zr[1] = dz;
+    glm_vec2_normalize(normal_zr);
+
+    for (int j = 0; j <= RESOLUTION_THETA; j++) {
+      float theta = 2.f * M_PIf * (float)j / (float)RESOLUTION_THETA;
+      float x = r * cosf(theta);
+      float y = r * sinf(theta);
+
+      float normal_x = normal_zr[1] * cosf(theta);
+      float normal_y = normal_zr[1] * sinf(theta);
+
+      struct zna_ray_origin_unit_vertex *v =
+          vertices + j + i * (RESOLUTION_THETA + 1);
+
+      v->position[0] = x;
+      v->position[1] = y;
+      v->position[2] = z;
+
+      v->normal[0] = normal_x;
+      v->normal[1] = normal_y;
+      v->normal[2] = normal_zr[0];
+
+      v->uv[0] = 0;
+      v->uv[1] = 0;
+    }
+  }
+}
+
+static void
+construct_elements(short *elements)
+{
+  ushort num_vertices_in_a_latitudinal_line = RESOLUTION_THETA + 1;
+  int max = 0;
+  int count = 0;
+  for (int i = 1; i < RESOLUTION_Z; i++) {
+    for (int j = 0; j < RESOLUTION_THETA; j++) {
+      ushort A = i * num_vertices_in_a_latitudinal_line + j;
+      ushort B = A + 1;
+      ushort C = A + num_vertices_in_a_latitudinal_line;
+      ushort D = B - num_vertices_in_a_latitudinal_line;
+      if (C > max) max = C;
+
+      elements[count++] = A;
+      elements[count++] = C;
+      elements[count++] = B;
+
+      elements[count++] = B;
+      elements[count++] = D;
+      elements[count++] = A;
+    }
+  }
+
+  //         j    j+1
+  //         .     .
+  // i-1 . . . . . D . .
+  //         .     .
+  //         .     .
+  // i . . . A . . B . .
+  //         .     .
+  //         .     .
+  // i+1 . . C . . . . .
+  //         .     .
+  //         .     .
+}
 
 void
 zna_ray_origin_unit_commit(struct zna_ray_origin_unit *self, struct zn_ray *ray,
@@ -10,14 +179,15 @@ zna_ray_origin_unit_commit(struct zna_ray_origin_unit *self, struct zn_ray *ray,
 {
   if (!self->base_unit->has_renderer_objects) return;
 
-  vec3 tip;
-  zn_ray_get_tip(ray, tip);
-  znr_gl_base_technique_gl_uniform_vector(self->base_unit->technique, 0, "tip",
-      ZGN_GL_BASE_TECHNIQUE_UNIFORM_VARIABLE_TYPE_FLOAT, 3, 1, tip);
+  mat4 local_model = GLM_MAT4_IDENTITY_INIT;
 
-  znr_gl_base_technique_gl_uniform_vector(self->base_unit->technique, 0,
-      "origin", ZGN_GL_BASE_TECHNIQUE_UNIFORM_VARIABLE_TYPE_FLOAT, 3, 1,
-      ray->origin);
+  glm_translate(local_model, ray->origin);
+  glm_rotate_y(
+      local_model, atan2(-ray->direction[0], -ray->direction[2]), local_model);
+  glm_rotate_x(local_model, asinf(ray->direction[1]), local_model);
+
+  znr_gl_base_technique_gl_uniform_matrix(self->base_unit->technique, 0,
+      "local_model", 4, 4, 1, false, local_model[0]);
 
   znr_virtual_object_commit(virtual_object);
 }
@@ -41,6 +211,8 @@ struct zna_ray_origin_unit *
 zna_ray_origin_unit_create(struct zna_system *system)
 {
   struct zna_ray_origin_unit *self;
+  struct zna_ray_origin_unit_vertex vertices[vertex_count];
+  short elements[element_count];
 
   self = zalloc(sizeof *self);
   if (self == NULL) {
@@ -48,35 +220,52 @@ zna_ray_origin_unit_create(struct zna_system *system)
     goto err;
   }
 
-  float vertices[2] = {0, 1};
+  construct_vertices(vertices);
+  construct_elements(elements);
 
   struct zgnr_mem_storage *vertex_buffer =
       zgnr_mem_storage_create(vertices, sizeof(vertices));
 
+  struct zgnr_mem_storage *element_array_buffer =
+      zgnr_mem_storage_create(elements, sizeof(elements));
+
   struct wl_array vertex_attributes;
   wl_array_init(&vertex_attributes);
 
-  struct zna_base_unit_vertex_attribute *vertex_attribute =
-      wl_array_add(&vertex_attributes, sizeof *vertex_attribute);
+  struct zna_base_unit_vertex_attribute *vertex_attribute;
+
+  vertex_attribute = wl_array_add(&vertex_attributes, sizeof *vertex_attribute);
   vertex_attribute->index = 0;
-  vertex_attribute->size = 1;
+  vertex_attribute->size = 3;
   vertex_attribute->type = GL_FLOAT;
   vertex_attribute->normalized = GL_FALSE;
-  vertex_attribute->stride = 0;
+  vertex_attribute->stride = sizeof(struct zna_ray_origin_unit_vertex);
   vertex_attribute->offset = 0;
 
+  vertex_attribute = wl_array_add(&vertex_attributes, sizeof *vertex_attribute);
+  vertex_attribute->index = 1;
+  vertex_attribute->size = 3;
+  vertex_attribute->type = GL_FLOAT;
+  vertex_attribute->normalized = GL_FALSE;
+  vertex_attribute->stride = sizeof(struct zna_ray_origin_unit_vertex);
+  vertex_attribute->offset =
+      offsetof(struct zna_ray_origin_unit_vertex, normal);
+
   union zgnr_gl_base_technique_draw_args draw_args;
-  draw_args.arrays.mode = GL_LINES;
-  draw_args.arrays.first = 0;
-  draw_args.arrays.count = 2;
+  draw_args.elements.mode = GL_TRIANGLES;
+  draw_args.elements.type = GL_UNSIGNED_SHORT;
+  draw_args.elements.offset = 0;
+  draw_args.elements.count = element_count;
 
   self->base_unit = zna_base_unit_create(system, ZNA_SHADER_RAY_VERTEX,
-      ZNA_SHADER_COLOR_FRAGMENT, vertex_buffer, &vertex_attributes,
-      ZGNR_GL_BASE_TECHNIQUE_DRAW_METHOD_ARRAYS, draw_args);
+      ZNA_SHADER_RAY_FRAGMENT, vertex_buffer, &vertex_attributes,
+      element_array_buffer, ZGNR_GL_BASE_TECHNIQUE_DRAW_METHOD_ELEMENTS,
+      draw_args);
 
   wl_array_release(&vertex_attributes);
 
   zgnr_mem_storage_unref(vertex_buffer);
+  zgnr_mem_storage_unref(element_array_buffer);
 
   return self;
 
--- zna/shaders/ray.vert
+++ zna/shaders/ray.vert
@@ -1,12 +1,21 @@
 #version 320 es
+precision mediump float;
 
-uniform mat4 zMVP;
-layout(location = 0) in float i;
-uniform vec3 origin;
-uniform vec3 tip;
+uniform mat4 zModel;
+uniform mat4 zView;
+uniform mat4 zProjection;
+uniform mat4 local_model;
+layout(location = 0) in vec4 local_position;
+layout(location = 1) in vec4 normal_in;
+
+out vec3 normal;
+out vec4 position;
 
 void
 main()
 {
-  gl_Position = zMVP * vec4(origin * (1.0 - i) + tip * i, 1);
+  vec3 origin = (zView * zModel * local_model * vec4(0.0, 0.0, 0.0, 1.0)).xyz;
+  position = zView * zModel * local_model * local_position;
+  gl_Position = zProjection * position;
+  normal = (zView * zModel * local_model * normal_in).xyz - origin;
 }
