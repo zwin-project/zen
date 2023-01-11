035301bb585a0414a8172adfa715e119258d216a
Aki-7 <aki.develop8128@gmail.com>
2023-01-12 02:12:34 +0900

    add bounded nameplate node

---
+++ zns/include/zns/bounded-nameplate.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cglm/types.h>
+
+struct zns_bounded;
+
+/**
+ * The life time of given zns_bounded must be longer than this object
+ */
+struct zns_bounded_nameplate {
+  struct zns_bounded *bounded;  // nonnull
+
+  struct zns_node *node;
+
+  struct {
+    float width;
+    float height;
+  } geometry;
+};
+
+/**
+ * returns transformation matrix relative to the world space
+ */
+void zns_bounded_nameplate_get_transform(
+    struct zns_bounded_nameplate *self, mat4 transform);
+
+struct zns_bounded_nameplate *zns_bounded_nameplate_create(
+    struct zns_bounded *bounded);
+
+void zns_bounded_nameplate_destroy(struct zns_bounded_nameplate *self);
