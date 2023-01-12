5a0b40e071cd26a4120aacbfb2d7a977e5ef6db8
Aki-7 <aki.develop8128@gmail.com>
2023-01-12 12:59:13 +0900

    draw constant title string

--- common/cairo.c
+++ common/cairo.c
@@ -2,6 +2,8 @@
 
 #include <math.h>
 
+#include "zen-common/log.h"
+
 void
 zn_cairo_draw_rounded_rectangle(
     cairo_t *cr, double x, double y, double width, double height, double radius)
@@ -15,4 +17,46 @@ zn_cairo_draw_rounded_rectangle(
   cairo_arc(cr, x + radius, y + height - radius, radius, M_PI / 2, M_PI);
   cairo_line_to(cr, x, y + radius);
   cairo_arc(cr, x + radius, y + radius, radius, M_PI, 3 * M_PI / 2);
+}
+
+void
+zn_cairo_draw_text(cairo_t *cr, char *text, double x, double y,
+    enum zn_cairo_anchor horizontal_anchor,
+    enum zn_cairo_anchor vertical_anchor)
+{
+  cairo_text_extents_t extents;
+  cairo_text_extents(cr, text, &extents);
+
+  x -= extents.x_bearing;
+
+  switch (horizontal_anchor) {
+    case ZN_CAIRO_ANCHOR_LEFT:
+      break;
+    case ZN_CAIRO_ANCHOR_CENTER:
+      x -= extents.width / 2;
+      break;
+    case ZN_CAIRO_ANCHOR_RIGHT:
+      x -= extents.width;
+      break;
+    default:
+      zn_error("invalid horizon anchor");
+      return;
+  }
+
+  switch (vertical_anchor) {
+    case ZN_CAIRO_ANCHOR_TOP:
+      y += extents.height;
+      break;
+    case ZN_CAIRO_ANCHOR_CENTER:
+      y += extents.height / 2;
+      break;
+    case ZN_CAIRO_ANCHOR_BOTTOM:
+      break;
+    default:
+      zn_error("invalid vertical anchor");
+      return;
+  }
+
+  cairo_move_to(cr, x, y);
+  cairo_show_text(cr, text);
 }
--- common/include/zen-common/cairo.h
+++ common/include/zen-common/cairo.h
@@ -2,5 +2,17 @@
 
 #include <cairo.h>
 
+enum zn_cairo_anchor {
+  ZN_CAIRO_ANCHOR_CENTER,
+  ZN_CAIRO_ANCHOR_LEFT,
+  ZN_CAIRO_ANCHOR_RIGHT,
+  ZN_CAIRO_ANCHOR_TOP,
+  ZN_CAIRO_ANCHOR_BOTTOM,
+};
+
 void zn_cairo_draw_rounded_rectangle(cairo_t *cr, double x, double y,
     double width, double height, double radius);
+
+void zn_cairo_draw_text(cairo_t *cr, char *text, double x, double y,
+    enum zn_cairo_anchor horizontal_anchor,
+    enum zn_cairo_anchor vertical_anchor);
