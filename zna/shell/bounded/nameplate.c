2725c656e6df9cc37c240ea7839f93352051e3c5
Aki-7 <aki.develop8128@gmail.com>
2023-01-12 12:59:13 +0900

    draw constant title string

--- zna/shell/bounded/nameplate.c
+++ zna/shell/bounded/nameplate.c
@@ -40,7 +40,12 @@ zna_bounded_nameplate_unit_update_texture(
   vec3 navy = ZN_NAVY_VEC3_INIT;
   cairo_set_source_rgba(cr, navy[0], navy[1], navy[2], 1.);
   zn_cairo_draw_rounded_rectangle(cr, 0, 0, width, height, 5.);
-  cairo_fill_preserve(cr);
+  cairo_fill(cr);
+  cairo_set_font_face(cr, zn_font_face_get_cairo_font_face(ZN_FONT_REGULAR));
+  cairo_set_source_rgba(cr, 1., 1., 1., 1.);
+  cairo_set_font_size(cr, height / 2);
+  zn_cairo_draw_text(cr, "Bounded App", 40, height / 2, ZN_CAIRO_ANCHOR_LEFT,
+      ZN_CAIRO_ANCHOR_CENTER);
 
   zna_base_unit_read_cairo_surface(self->base_unit, surface);
   znr_gl_sampler_parameter_i(
