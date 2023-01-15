7151c2a2dc771f8868f898afd4cd275f2777e968
watasuke102 <watasuke102@gmail.com>
2023-01-15 13:57:36 +0900

    add `zn_wlr_fbox_contains_point`

--- include/zen/wlr/box.h
+++ include/zen/wlr/box.h
@@ -4,3 +4,5 @@
 
 void zn_wlr_fbox_closest_point(const struct wlr_fbox *box, double x, double y,
     double *dest_x, double *dest_y);
+
+bool zn_wlr_fbox_contains_point(const struct wlr_fbox *box, double x, double y);
