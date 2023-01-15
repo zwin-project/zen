5c146e58748065808bf70e9c7e4f5908fa8902c3
watasuke102 <watasuke102@gmail.com>
2023-01-15 13:58:13 +0900

    add `zn_cairo_stamp_svg_on_surface`

--- common/cairo.c
+++ common/cairo.c
@@ -1,5 +1,6 @@
 #include "zen-common/cairo.h"
 
+#include <librsvg/rsvg.h>
 #include <math.h>
 
 #include "zen-common/log.h"
@@ -59,4 +60,35 @@ zn_cairo_draw_text(cairo_t *cr, char *text, double x, double y,
 
   cairo_move_to(cr, x, y);
   cairo_show_text(cr, text);
+}
+
+bool
+zn_cairo_stamp_svg_on_surface(cairo_t *cr, const char *filename, double x,
+    double y, double width, double height)
+{
+  GError *error = NULL;
+  GFile *file = g_file_new_for_path(filename);
+  RsvgHandle *handle = rsvg_handle_new_from_gfile_sync(
+      file, RSVG_HANDLE_FLAGS_NONE, NULL, &error);
+  g_object_unref(file);
+  if (handle == NULL) {
+    zn_error("Failed to create the svg handler: %s", error->message);
+    g_error_free(error);
+    return false;
+  }
+
+  RsvgRectangle viewport = {
+      .x = x,
+      .y = y,
+      .width = width,
+      .height = height,
+  };
+
+  if (!rsvg_handle_render_document(handle, cr, &viewport, &error)) {
+    zn_error("Failed to render the svg: %s", error->message);
+    g_error_free(error);
+    return false;
+  }
+  g_object_unref(handle);
+  return true;
 }
