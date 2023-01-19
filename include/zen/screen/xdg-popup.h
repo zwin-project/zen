4f02927a07cb37a113a49114f30dc6bfbcc8b7b6
watasuke102 <watasuke102@gmail.com>
2023-01-19 22:54:45 +0900

    append child popup

--- include/zen/screen/xdg-popup.h
+++ include/zen/screen/xdg-popup.h
@@ -14,6 +14,7 @@ struct zn_xdg_popup {
 
   struct wl_listener map_listener;
   struct wl_listener unmap_listener;
+  struct wl_listener new_popup_listener;
   struct wl_listener wlr_xdg_surface_destroy_listener;
 };
 
