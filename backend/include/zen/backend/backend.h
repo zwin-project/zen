2b4628b68e41f1bcf6cf6059415d470c959d17a3
Aki-7 <aki.develop8128@gmail.com>
2023-02-11 00:14:04 +0900

    introduce wlr_backend

--- backend/include/zen/backend/backend.h
+++ backend/include/zen/backend/backend.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <wlr/backend.h>
+
 #include "zen/backend.h"
 
-struct zn_wlr_backend {
+struct zn_default_backend {
   struct zn_backend base;
 
   struct wl_display *display;  // @nonnull, @outlive
+
+  struct wlr_backend *wlr_backend;  // @nonnull, @owning
 };
