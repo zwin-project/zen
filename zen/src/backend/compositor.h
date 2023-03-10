e5f09bc7d5dec6f26dbec4a33f2a6b60c35a54db
Aki-7 <aki.develop8128@gmail.com>
2023-03-10 16:04:22 +0900

    add necessary globals

--- zen/src/backend/compositor.h
+++ zen/src/backend/compositor.h
@@ -2,12 +2,16 @@
 
 #include <wlr/render/wlr_renderer.h>
 #include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/xwayland.h>
 
 struct zn_compositor {
   // These objects will be automatically destroyed when wl_display is destroyed
-  struct wlr_compositor *wlr_compositor;       // @nonnull, @outlive
-  struct wlr_layer_shell_v1 *wlr_layer_shell;  // @nonnull, @outlive
+  struct wlr_compositor *wlr_compositor;                // @nonnull, @outlive
+  struct wlr_xdg_shell *xdg_shell;                      // @nonnull, @outlive
+  struct wlr_layer_shell_v1 *wlr_layer_shell;           // @nonnull, @outlive
+  struct wlr_data_device_manager *data_device_manager;  // @nonnull, @outlive
 
   struct wlr_xwayland *xwayland;  // @nonnull, @owning
 
