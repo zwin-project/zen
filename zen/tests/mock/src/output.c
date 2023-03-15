e5b1cc30bf3f4a4a5b2dfe950e8f68dbdaf3a954
Aki-7 <aki.develop8128@gmail.com>
2023-03-15 15:43:21 +0900

    update mock output

--- zen/tests/mock/src/output.c
+++ zen/tests/mock/src/output.c
@@ -5,6 +5,7 @@
 
 #include "zen-common/util.h"
 #include "zen/screen.h"
+#include "zen/snode.h"
 
 static void
 zn_mock_output_damage(void *impl_data, struct wlr_fbox *damage_box UNUSED)
@@ -14,8 +15,16 @@ zn_mock_output_damage(void *impl_data, struct wlr_fbox *damage_box UNUSED)
       (int)damage_box->y, (int)damage_box->width, (int)damage_box->height);
 }
 
+static struct zn_snode *
+zn_mock_output_get_layer(void *impl_data, enum zwlr_layer_shell_v1_layer layer)
+{
+  struct zn_mock_output *self = impl_data;
+  return self->layers[layer];
+}
+
 static const struct zn_screen_interface screen_implementation = {
     .damage = zn_mock_output_damage,
+    .get_layer = zn_mock_output_get_layer,
 };
 
 bool
@@ -42,6 +51,10 @@ zn_mock_output_create(float width, float height)
   struct zn_mock_output *self = zalloc(sizeof *self);
   assert(self);
 
+  for (int i = 0; i < 4; i++) {
+    self->layers[i] = zn_snode_create(self, &zn_snode_noop_implementation);
+  }
+
   self->screen = zn_screen_create(self, &screen_implementation);
   assert(self->screen);
 
@@ -55,6 +68,9 @@ zn_mock_output_create(float width, float height)
 void
 zn_mock_output_destroy(struct zn_mock_output *self)
 {
+  for (int i = 0; i < 4; i++) {
+    zn_snode_destroy(self->layers[i]);
+  }
   pixman_region32_fini(&self->damage);
   zn_screen_destroy(self->screen);
   free(self);
